eb8d48f399fc732971184673205f7d3d46b8fb8e
Bernardo Generozo <bernardo.generozo@gmail.com>
2019-02-20 18:46:10 -0300

    Adicionado metodo para salvar relação de comentario com tagbook web

--- app/Flows/SaveFlows/TagBookWebAttributeSaveFlow.php
+++ app/Flows/SaveFlows/TagBookWebAttributeSaveFlow.php
@@ -3,6 +3,7 @@
 namespace App\Flows\SaveFlows;
 
 use Illuminate\Support\Arr;
+use App\Model\AttributeComment;
 
 class TagBookWebAttributeSaveFlow extends AbstractSaveFlow
 {
@@ -10,5 +11,13 @@ class TagBookWebAttributeSaveFlow extends AbstractSaveFlow
     {
         $this->model->fill($this->input);
         $this->model->save();
+
+        $comments = array_values(Arr::get($this->input, 'attribute-comments', []));
+        $this->saveRelationHasMany(
+            $comments,
+            AttributeComment::class,
+            AttributeCommentSaveFlow::class,
+            'tag_book_web_attribute_id'
+        );
     }
 }
