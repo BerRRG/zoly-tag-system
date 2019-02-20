eca6de24150d5c519c5bbb9d19456c68cda752c2
Bernardo Generozo <bernardo.generozo@gmail.com>
2019-02-20 18:44:52 -0300

    Adicionado metodo de salvar relações many ao saveflow

--- app/Flows/SaveFlows/AbstractSaveFlow.php
+++ app/Flows/SaveFlows/AbstractSaveFlow.php
@@ -2,6 +2,8 @@
 
 namespace App\Flows\SaveFlows;
 
+use Illuminate\Support\Arr;
+
 abstract class AbstractSaveFlow
 {
     protected $model;
@@ -14,4 +16,25 @@ abstract class AbstractSaveFlow
     }
 
     abstract public function save();
+
+    protected function saveRelationHasMany($objects, $modelClass, $modelSaveFlow, $relationKey)
+    {
+        foreach($objects as $key => $object) {
+            $model = new $modelClass();
+
+            if (Arr::get($object, 'id')) {
+                $model = $model::find(Arr::get($object, 'id'));
+            }
+
+            Arr::set($object, 'order', $key);
+            Arr::set($object, $relationKey, $this->model->id);
+
+            $modelSaveFlow = new $modelSaveFlow(
+                $model,
+                $object
+            );
+
+            $modelSaveFlow->save();
+        }
+    }
 }
