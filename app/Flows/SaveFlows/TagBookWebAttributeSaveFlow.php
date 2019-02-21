87dc3d17ca40f47f52614929acf1eee98e77ce96
Bernardo Generozo <bernardo.generozo@gmail.com>
2019-02-21 17:04:29 -0300

    Adicionado custom dimension

--- app/Flows/SaveFlows/TagBookWebAttributeSaveFlow.php
+++ app/Flows/SaveFlows/TagBookWebAttributeSaveFlow.php
@@ -4,6 +4,7 @@ namespace App\Flows\SaveFlows;
 
 use Illuminate\Support\Arr;
 use App\Model\AttributeComment;
+use App\Model\CustomDimension;
 
 class TagBookWebAttributeSaveFlow extends AbstractSaveFlow
 {
@@ -17,6 +18,14 @@ class TagBookWebAttributeSaveFlow extends AbstractSaveFlow
             $comments,
             AttributeComment::class,
             AttributeCommentSaveFlow::class,
+            'tag_book_web_attribute_id'
+        );
+
+        $dimensions = array_values(Arr::get($this->input, 'attribute-dimensions', []));
+        $this->saveRelationHasMany(
+            $dimensions,
+            CustomDimension::class,
+            CustomDimensionSaveFlow::class,
             'tag_book_web_attribute_id'
         );
     }
