6fd6ea320fdc4702197af7c211f235e023b7b86a
Bernardo Generozo <bernardo.generozo@gmail.com>
2019-02-22 16:53:14 -0300

    Modificado template de export de doc para web attribute

--- resources/views/exports/partials/web-attributes-body.blade.php
+++ resources/views/exports/partials/web-attributes-body.blade.php
@@ -14,12 +14,31 @@
     <td>{{ $attribute->status_google_analytics }}</td>
     <td>{{ $attribute->track_type }}</td>
     <td>{{ $attribute->tag_name }}</td>
-    <td>{{ $attribute->fields_to_set }}</td>
+    <td>
+        {{ $attribute->fields_to_set_no_interaction }}
+        <br />
+        {{ $attribute->fields_to_set_type }}
+        <br />
+        {{ $attribute->fields_to_set_example }}
+    </td>
     <td>{{ $attribute->event_category }}</td>
     <td>{{ $attribute->event_action }}</td>
     <td>{{ $attribute->event_label_var }}</td>
     <td>{{ $attribute->event_value }}</td>
-    <td>{{ $attribute->custom_dimension_metrics }}</td>
+    <td>
+        @foreach($attribute->dimensions() as $dimension)
+            {{ $dimension->label }}
+            <br />
+        @endforeach
+    </td>
     <td>{{ $attribute->additional }}</td>
-    <td>{{ $attribute->comments }}</td>
+    <td>
+        @foreach($attribute->comments()->get() as $comment)
+            {{ $comment->variable}} - {{ $comment->description }} Ex: {{$comment->example}}
+            <br />
+            @if ($comment->note)
+                OBS: {{$comment->note}}
+            @endif
+        @endforeach
+    </td>
 </tr>
